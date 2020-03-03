7bc3682a7b6bfcd372ad9841afef82d90b3f6374
kohayan <disney.2381509@gmail.com>
2020-03-03 06:22:05 +0000

    [Finish] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2e132d0230301c36b41c3f0ce6c02a7ab35f150234e7463d06197884fe634aecd3ea13b23f204145a26a344e7b58db0f3efb7b639574a423ed6c47903e4b6610'
+
